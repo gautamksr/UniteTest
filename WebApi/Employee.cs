f1ae8849880f26b89de0c6d19711641132251c3a
gautam.sharma <gautam.sharma@atos.net>
2020-07-28 12:05:09 +0000

    Intial api project

---
+++ WebApi/Employee.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApi
+{
+    public class Employee : IEmployee
+    {
+        private readonly IsqlEmployee isql;
+
+        public Employee(IsqlEmployee isql)
+        {
+            this.isql = isql;
+        }
+
+        public List<string> GetEmployee()
+        {
+            return isql.GetEmployee();
+        }
+
+        public async Task<List<string>> GetEmployeeAscn()
+        {
+            return await isql.GetEmployeeAsync();
+        }
+    }
+
+    public interface IEmployee
+    {
+
+        List<string> GetEmployee();
+
+        Task<List<string>> GetEmployeeAscn();
+    }
+}
---
+++ WebApi/SqlEmployee.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApi
+{
+    public class SqlEmployee : IsqlEmployee
+    {
+        public List<string> GetEmployee()
+        {
+            return new List<string> { "emp1", "emp2", "emp3" };
+        }
+
+        public async Task<List<string>> GetEmployeeAsync()
+        {
+            var empList = new List<string> { "emp1", "emp2", "emp3" };
+            var empList1 = await Task.FromResult<List<string>>(empList);
+            return empList1;
+        }
+    }
+
+    public interface IsqlEmployee
+    {
+        List<string> GetEmployee();
+        Task<List<string>> GetEmployeeAsync();
+    }
+}
